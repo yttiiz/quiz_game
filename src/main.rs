8aa941e493daaca9cb6ea006d2ef5d3050176e18
yttiiz <dominique.talis@proton.me>
2024-07-27 01:01:55 +0200

    feat(mongodb): create mongodb & env variables modules | change main function to async future

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
-use quiz::game_object::Quiz;
+use futures::TryStreamExt;
+use quiz::{
+    db_connexion::Mongo,
+    game_object::Quiz
+};
+use std::error::Error;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mongo = Mongo::new().await?;
+    let mut cursor = mongo.cursor().await?;
     let mut quiz = Quiz::new("Connaissez-vous la Guadeloupe ?");
+
+    // Add questions
+    while let Some(question) = cursor.try_next().await? {
+        quiz.add_question(question);
+    }
+
     quiz.start();
+
+    Ok(())
 }
